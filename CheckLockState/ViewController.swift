d609f8d57e7350a4526f765d8a28c237cbb82bd9
Amirreza Eghtedari <eghtedari.amir@gmail.com>
2020-04-04 15:36:09 +0430

    Initial Commit

---
+++ CheckLockState/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckLockState
+//
+//  Created by Amirreza Eghtedari on 16/1/1399AP.
+//  Copyright © 1399 AP Amirreza Eghtedari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
